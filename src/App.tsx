5e532ed5c835f9e952a5b1ed87b259efda56057d
Marcaus.Cruz <marcauscruz@gmail.com>
2025-11-06 13:39:22 -0800

    Create Generic Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import InputWrapper from "./components/InputWrapper";
 import Button from "./components/Button";
+import Container from "./components/Container";
+import InputWrapper from "./components/InputWrapper";
 import "./index.css";
 
 function App() {
   return (
     <main>
+      <Container as={Button} el="button">
+        Click me
+      </Container>
       <InputWrapper id="name" label="Your name:" />
       <InputWrapper id="age" label="Your age:" type="number" />
       <p>
